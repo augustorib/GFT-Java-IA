5550e681c02c3037076009c14aa8455985a2d5f5
augustorib <augustorifra@gmail.com>
2024-07-19 03:51:18 -0300

    feat: Initial commit

---
+++ src/main/java/com/agustin/GftJavaIaApplication.java
@@ -0,0 +1,13 @@
+package com.agustin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GftJavaIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GftJavaIaApplication.class, args);
+	}
+
+}
